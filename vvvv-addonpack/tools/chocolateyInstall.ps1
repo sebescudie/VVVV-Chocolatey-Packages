40b872bbcdce278c98b0e1a5617fe2828374bf72
sebescudie <seb.espm@gmail.com>
2018-07-29 18:09:07 +0200

    started working on addonpack chocoinstall

--- vvvv-addonpack/tools/chocolateyInstall.ps1
+++ vvvv-addonpack/tools/chocolateyInstall.ps1
@@ -1,4 +1,21 @@
-$binRoot = "$env:systemdrive\"
-if($env:chocolatey_bin_root -ne $null){$binRoot = $env:chocolatey_bin_root}
-$installPath = Join-Path $binRoot 'vvvv\vvvv_45beta29_x86'
-Install-ChocolateyZipPackage vvvv-addonpack "http://vvvv.org/sites/all/modules/general/pubdlcnt/pubdlcnt.php?file=http://vvvv.org/sites/default/files/addons_45beta29_x86.zip&nid=93333" $installPath
+$ErrorActionPreference = 'Stop';
+# Find if user forces x86 or not and set $toolsDir accordingly
+$toolsDir   = Join-Path $env:SystemDrive 'vvvv' 'vvvv_50beta36_x64'
+$url        = 'https://vvvv.org/sites/all/modules/general/pubdlcnt/pubdlcnt.php?file=https://vvvv.org/sites/default/files/addons_50beta36_x86.zip&nid=1'
+$url64      = 'https://vvvv.org/sites/all/modules/general/pubdlcnt/pubdlcnt.php?file=https://vvvv.org/sites/default/files/addons_50beta36_x64.zip&nid=1'
+
+$packageArgs = @{
+  packageName   = 'vvvv-addonpack'
+  unzipLocation = $toolsDir
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'vvvv'
+
+  checksum      = '54645F17A79B633CB19BFCB246F8825F8DB636232F9B803FED0E8E8BCC54E8F5'
+  checksumType  = 'sha256'
+  checksum64    = '6FA2A6DF21A0EF1E35A485E732898DBA4E9F413F20300ED046CA84F2A560CFDB'
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs

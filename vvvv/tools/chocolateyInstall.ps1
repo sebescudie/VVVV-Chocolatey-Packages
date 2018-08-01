24a2dd9a06af34c7d56cb6a2847937ad35670213
sebescudie <seb.espm@gmail.com>
2018-08-01 13:53:07 +0200

    implemented first cmd arg parsing

--- vvvv/tools/chocolateyInstall.ps1
+++ vvvv/tools/chocolateyInstall.ps1
@@ -1,7 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
-$toolsDir   = Join-Path $env:SystemDrive 'vvvv'
+
 $url        = 'https://vvvv.org/sites/all/modules/general/pubdlcnt/pubdlcnt.php?file=https://vvvv.org/sites/default/files/vvvv_50beta36_x86.zip&nid=1'
 $url64      = 'https://vvvv.org/sites/all/modules/general/pubdlcnt/pubdlcnt.php?file=https://vvvv.org/sites/default/files/vvvv_50beta36_x64.zip&nid=1'
+$pp         = Get-PackageParameters
+
+# Check if user has passed a custom InstallationPath
+if(!$pp.InstallationPath) { $pp.InstallationPath = "$env:SystemDrive\vvvv"}
+$toolsDir   = $pp.InstallationPath
 
 $packageArgs = @{
   packageName   = 'vvvv'
@@ -17,5 +22,6 @@ $packageArgs = @{
   checksum64    = '6FA2A6DF21A0EF1E35A485E732898DBA4E9F413F20300ED046CA84F2A560CFDB'
   checksumType64= 'sha256'
 }
+
 
 Install-ChocolateyZipPackage @packageArgs
